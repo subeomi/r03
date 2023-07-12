b4e68182503652f030ca9bb7b5ee31c6646a0417
subeomi <tnqjarnqja@gmail.com>
2023-07-12 17:15:30 +0900

    0712

---
+++ src/components/ProductInput.js
@@ -0,0 +1,68 @@
+import { useRef, useState } from "react";
+import { postProduct } from "../api/productAPI";
+
+const initState = {
+    pname:'Ice Coffee',
+    pdesc:'Coffee....',
+    price:4000
+}
+
+const ProductInput = () => {
+
+    const fileRef = useRef()
+    const [product, setProduct] = useState({...initState})
+
+    const handleChange = (e) => {
+        product[e.target.name] = e.target.value
+        setProduct({...product})
+    }
+
+    const handleClickSave = (e) => {
+        console.log(product)
+
+        const formData = new FormData()
+
+        formData.append("pname", product.pname)
+        formData.append("pdesc", product.pdesc)
+        formData.append("price", product.price)
+
+        console.dir(fileRef.current)
+
+        const arr = fileRef.current.files
+
+        for(let file of arr){
+            formData.append("files", file)
+        }
+
+        postProduct(formData)
+
+    }
+
+    const handleClickClear = (e) => {
+        fileRef.current.value = ''
+    }
+
+    return ( 
+        <div>
+            <h1>Input</h1>
+            <div>
+                <input type="text" name="pname" value={product.pname} onChange={handleChange}></input>
+            </div>
+            <div>
+                <input type="text" name="pdesc" value={product.pdesc} onChange={handleChange}></input>
+            </div>
+            <div>
+                <input type="number" name="price" value={product.price} onChange={handleChange}></input>
+            </div>
+            <div>
+                <input type="file" ref={fileRef} multiple name="images" onChange={handleChange}></input>
+            </div>
+            <div>
+                <button onClick={handleClickSave}>SAVE</button>
+                <button onClick={handleClickClear}>CLEAR FILES</button>
+            </div>
+        </div>
+     );
+}
+ 
+export default ProductInput;
